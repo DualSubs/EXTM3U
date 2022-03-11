23469bfbca3d25b5a466c4881825751e38071e9e
Virgil Clyne <Virgil@nanocat.me>
2022-03-11 11:51:25 +0800

    Add EXTM3U.parse
    Update EXTM3U.beta.js

--- EXTM3U.beta.js
+++ EXTM3U.beta.js
@@ -1,4 +1,5 @@
 // refer: https://datatracker.ietf.org/doc/html/draft-pantos-http-live-streaming-08
+// refer: https://datatracker.ietf.org/doc/html/draft-pantos-http-live-streaming-08
 function EXTM3U(name, opts) {
 	return new (class {
 		constructor(name, opts) {
@@ -8,16 +9,36 @@ function EXTM3U(name, opts) {
 
 		parse(m3u8 = new String, options = []) {
 			$.log(`🚧 ${$.name}, parse EXTM3U`, "");
-			const headers_EXTM3U_Regex = /^#(?<fileType>EXTM3U)?[^]*/;
-			const headers_Option_Regex = /^#(?<Xoptions>EXT[^:]+)$/m;
-			const body_Regex = /^(?<XType>EXT.*):(?<Xoptions>.+)[^]?(?<URI>.+)?$/;
+			const EXTM3U_headers_Regex = /^#(?<fileType>EXTM3U)?[^]*/;
+			const EXTM3U_option_Regex = /^#(?<Options>EXT[^:]+)$/m;
+			const EXTM3U_body_Regex = /^(?<EXT>EXT[^:]+):(?<OPTION>.+)[^]?(?<URI>.+)?$/;
             let json = {
-				headers: m3u8.match(headers_EXTM3U_Regex)?.groups ?? null,
-				option: m3u8.match(headers_Option_Regex)?.groups ?? null,
-				body: m3u8.split(/[(\r\n)\r\n]#/).map(item => item = item.match(body_Regex)?.groups ?? "")
+				headers: m3u8.match(EXTM3U_headers_Regex)?.groups ?? null,
+				option: m3u8.match(EXTM3U_option_Regex)?.groups ?? null,
+				body: m3u8.split(/[(\r\n)\r\n]#/).map(item => item = item.match(EXTM3U_body_Regex)?.groups ?? null)
 			};
 			$.log(`🚧 ${$.name}, parse WebVTT`, `json.headers: ${JSON.stringify(json.headers)}`, "");
 			$.log(`🚧 ${$.name}, parse WebVTT`, `json.option: ${JSON.stringify(json.option)}`, "");
+			//$.log(`🚧 ${$.name}, parse WebVTT`, `json.body: ${JSON.stringify(json.body)}`, "");
+			json.body = json.body.map(item => {
+				if (item?.OPTION) {
+					item.OPTION = Object.fromEntries(item.OPTION.replace(/\"/g, "").split(",").map(item => item.split("=")));
+					$.log(`🚧 ${$.name}, parse WebVTT`, `item.OPTION ${JSON.stringify(item.OPTION)}`, "");
+				}
+				$.log(`🚧 ${$.name}, parse WebVTT`, `item ${JSON.stringify(item)}`, "");
+				return item
+				/*
+				if (item?.OPTION) {
+					$.log(`🚧 ${$.name}, parse WebVTT`, `item.OPTION: ${item?.OPTION}`, "");
+					item.OPTION = item.OPTION.split(",")
+					$.log(`🚧 ${$.name}, parse WebVTT`, `item.OPTION.split: ${item?.OPTION}`, "");
+					item.OPTION = item.OPTION.map(item => item.replace(/\"/, ""))
+					$.log(`🚧 ${$.name}, parse WebVTT`, `item.OPTION.map: ${item?.OPTION}`, "");
+					item.OPTION = item.OPTION.map(item => item.split("="))
+					$.log(`🚧 ${$.name}, parse WebVTT`, `item.OPTION.map ${item?.OPTION}`, "");
+				}
+				*/
+			});
 			$.log(`🚧 ${$.name}, parse WebVTT`, `json.body: ${JSON.stringify(json.body)}`, "");
 			return json
 		};
