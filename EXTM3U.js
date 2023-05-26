1d6e4f5d8d523bf4e3ed1a92b67aba6373ba5fb0
Virgil Clyne <Virgil@nanocat.me>
2023-05-27 07:44:06 +0800

    build(js): v0.8.1
    Update EXTM3U.embedded.js
    Update EXTM3U.embedded.min.js

--- EXTM3U.js
+++ EXTM3U.js
@@ -1,39 +1,48 @@
 // refer: https://datatracker.ietf.org/doc/html/draft-pantos-http-live-streaming-08
 export class EXTM3U {
 	constructor(opts) {
-		this.name = "EXTM3U v0.7.2";
+		this.name = "EXTM3U v0.8.1";
 		this.opts = opts;
 		this.newLine = (this.opts.includes("\n")) ? "\n" : (this.opts.includes("\r")) ? "\r" : (this.opts.includes("\r\n")) ? "\r\n" : "\n";
 	};
 
+
 	parse(m3u8 = new String) {
-		//$.log(`🚧 ${$.name}, parse EXTM3U`, "");
+		console.log(`☑️ ${this.name}, parse EXTM3U`, "");
 		/***************** v0.7.0-beta *****************/
 		const EXTM3U_Regex = /^(?<TYPE>(?:EXT|AIV)[^#:]+):?(?<OPTION>.+)?[\r\n]?(?<URI>.+)?$/;
-		let json = m3u8.replace(/\r\n/g, "\n").split(/[\r\n]+#/).map(v => v.match(EXTM3U_Regex)?.groups ?? v)
-		//$.log(`🚧 ${$.name}, parse EXTM3U`, `json: ${JSON.stringify(json)}`, "");
-		json = json.map(item => {
-			//$.log(`🚧 ${$.name}, parse EXTM3U`, `before: item.OPTION.split(/,(?=[A-Z])/) ${JSON.stringify(item.OPTION?.split(/,(?=[A-Z])/) ?? "")}`, "");
-			if (/=/.test(item?.OPTION) && this.opts.includes(item.TYPE)) item.OPTION = Object.fromEntries(item.OPTION.split(/,(?=[A-Z])/).map(item => item.split(/=(.*)/)));
+		let json = m3u8.replace(/\r\n/g, "\n").split(/[\r\n]+#/).map(v => v.match(EXTM3U_Regex)?.groups ?? v).map(item => {
+			if (/=/.test(item?.OPTION)) item.OPTION = Object.fromEntries(`${item.OPTION}\,`.split(/,\s*(?![^"]*",)/).slice(0, -1).map(option => {
+				option = option.split(/=(.*)/);
+				option[1] = (isNaN(option[1])) ? option[1].replace(/^"(.*)"$/, "$1") : parseInt(option[1], 10);
+				return option;
+			}));
 			return item
 		});
-		//$.log(`🚧 ${$.name}, parse WebVTT`, `json: ${JSON.stringify(json)}`, "");
+		console.log(`✅ ${this.name}, parse WebVTT`, `json: ${JSON.stringify(json)}`, "");
 		return json
 	};
 
 	stringify(json = new Array) {
-		//$.log(`🚧 ${$.name}, stringify EXTM3U`, "");
+		console.log(`☑️ ${this.name}, stringify EXTM3U`, "");
 		if (!json?.[0]?.includes("#EXTM3U")) json.unshift("#EXTM3U")
+		const OPTION_value_Regex = /^((-?\d+[x.\d]+)|[0-9A-Z-]+)$/;
 		let m3u8 = json.map(item => {
-			if (typeof item?.OPTION == "object") item.OPTION = Object.entries(item.OPTION).map(item => item = item.join("=")).join(",");
+			console.log(`🚧 ${this.name}, stringify EXTM3U`, `before: item.OPTION ${JSON.stringify(item.OPTION)}`, "");
+			if (typeof item?.OPTION == "object") item.OPTION = Object.entries(item.OPTION).map(option => {
+				if (item.TYPE === "EXT-X-SESSION-DATA") option[1] = `"${option[1]}"`;
+				else if (!isNaN(option[1])) option[1] = (typeof option[1] === "number") ? option[1] : `"${option[1]}"`;
+				else if (option[0] === "INSTREAM-ID") option[1] = `"${option[1]}"`;
+				else if (!OPTION_value_Regex.test(option[1])) option[1] = `"${option[1]}"`;
+				return option.join("=");
+			}).join(",");
 			/***************** v0.7.0-beta *****************/
-			return item = (item?.URI) ? item.TYPE + ":" + item.OPTION + this.newLine + item.URI
-				: (item?.OPTION) ? item.TYPE + ":" + item.OPTION
-					: (item?.TYPE) ? item.TYPE
+			return item = (item.URI) ? item.TYPE + ":" + item.OPTION + this.newLine + item.URI
+				: (item.OPTION) ? item.TYPE + ":" + item.OPTION
+					: (item.TYPE) ? item.TYPE
 						: item
-		})
-		m3u8 = m3u8.join(this.newLine + "#")
-		//$.log(`🚧 ${$.name}, stringify EXTM3U`, `m3u8: ${m3u8}`, "");
+		}).join(this.newLine + "#");
+		console.log(`✅ ${this.name}, stringify EXTM3U`, `m3u8: ${m3u8}`, "");
 		return m3u8
 	};
-}
+};
